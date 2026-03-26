4a0b3565389efbbc97cbc0aef894ad5a4690f341
Lunitaisa <lunaisabellasson@gmail.com>
2026-03-26 16:48:03 -0500

    Refactor CuentaBancaria and remove Estudiante class

--- Corte1/Java/Cuentabancaria/CuentaBancaria.java
+++ Corte1/Java/Cuentabancaria/CuentaBancaria.java
@@ -28,69 +28,17 @@ class CuentaBancaria {
         System.out.println("Saldo = " + saldo);
     }
 }
+public class MainCuenta {
+ public static void main(String[] args) {
+ CuentaBancaria c1 = new CuentaBancaria("Maria", 100000.0);
+ c1.mostrar();
 
-class Estudiante {
-    private String nombre;
-    private double nota;
+ c1.depositar(25000.0);
+ c1.retirar(10000.0);
 
-    public Estudiante(String nombre, double nota) {
-        this.nombre = nombre;
-        if (nota >= 0) {
-            this.nota = nota;
-        } else {
-            this.nota = 0;
-        }
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public double getNota() {
-        return nota;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setNota(double nota) {
-        if (nota >= 0) {
-            this.nota = nota;
-        } else {
-            System.out.println("No se permite una nota negativa");
-        }
-    }
-
-    public void mostrar() {
-        System.out.println("Nombre = " + nombre);
-        System.out.println("Nota = " + nota);
-    }
+ System.out.println("\nDespues de operaciones:");
+ c1.mostrar();
+ }
 }
 
-public class Practica4 {
-    public static void main(String[] args) {
-        System.out.println("=== EJEMPLO: CUENTA BANCARIA ===");
-        CuentaBancaria c1 = new CuentaBancaria("Maria", 100000.0);
-        c1.mostrar();
-
-        c1.depositar(25000.0);
-        c1.retirar(10000.0);
-
-        System.out.println("\nDespues de operaciones:");
-        c1.mostrar();
-
-        System.out.println("\n=== EJERCICIO: ESTUDIANTE ===");
-        Estudiante e1 = new Estudiante("Carlos", 85);
-        e1.mostrar();
-
-        System.out.println("\nActualizando datos:");
-        e1.setNombre("Carlos Perez");
-        e1.setNota(95);
-        e1.mostrar();
-
-        System.out.println("\nIntentando asignar una nota negativa:");
-        e1.setNota(-10);
-        e1.mostrar();
-    }
-}
+   
