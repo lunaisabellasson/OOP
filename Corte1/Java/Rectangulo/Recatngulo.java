773611c8dac2e7dc3cc008c71afbabbd69b18815
Lunitaisa <lunaisabellasson@gmail.com>
2026-03-26 17:15:14 -0500

    Add files via upload

---
+++ Corte1/Java/Rectangulo/Recatngulo.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        Rectangulo r1 = new Rectangulo(3.0, 2.0);
+
+        System.out.println("Base: " + r1.base);
+        System.out.println("Altura: " + r1.altura);
+        System.out.println("Area: " + r1.area());
+    }
+}
+
+class Rectangulo {
+    public double base;
+    public double altura;
+
+    public Rectangulo(double b, double h) {
+        base = b;
+        altura = h;
+    }
+
+    public double area() {
+        return base * altura;
+    }
+}
